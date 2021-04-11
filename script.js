1ec07b82f3aeacb3082001496e3c7316fe98b9b5
Fess353 <80582623+Fess353@users.noreply.github.com>
2021-04-11 20:40:59 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,36 @@
-let point2=document.querySelector('.point2');
-let point3=document.querySelector('.soft_names');
+let point1 = document.querySelector('.point1');
+let point2 = document.querySelector('.point2');
+let point3 = document.querySelector('.point3');
+let point4 = document.querySelector('.point4');
 
+var ham = document.querySelector('.ham');
+var nav = document.querySelector('.navigation_menu');
 
-point2.onclick = function () {
-    window.scrollTo(0, 750 );
+point1.onclick = function () {
+    window.scrollTo(0, 150 );
     behavior: 'smooth';
 };
 
-var skill_nav = document.getElementsByClassName('.skill_main');
-console.log(skill_nav);
+point2.onclick = function () {
+    window.scrollTo(0, 650 );
+    behavior: 'smooth';
+};
+
+point3.onclick = function () {
+    window.scrollTo(0, 1150 );
+    behavior: 'smooth';
+};
+
+point4.onclick = function () {
+    window.scrollTo(0, 1600 );
+    behavior: 'smooth';
+};
+
+ham.onclick = function() {
+    nav.classList.toggle('hidden');
+};
+
+
 
 //const el = document.getElementsByClassName('.soft_star1');
 //el.scrollIntoView({block: "center", inline: "center"}); // Задаём параметр block для вертикальной прокрутки до центра, inline до горизонтальной
