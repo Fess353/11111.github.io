7c8c6f8b42d1b53ca9e1383ec2273c74441b669e
Fess353 <80582623+Fess353@users.noreply.github.com>
2021-04-19 09:33:58 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,36 +1,43 @@
-let point1 = document.querySelector('.point1');
-let point2 = document.querySelector('.point2');
-let point3 = document.querySelector('.point3');
-let point4 = document.querySelector('.point4');
+var point1 = document.querySelector('.point1');
+var point2 = document.querySelector('.point2');
+var point3 = document.querySelector('.point3');
+var point4 = document.querySelector('.point4');
+
+
+var point2_aim = document.getElementById("point2_aim");
+var point3_aim = document.getElementById("point3_aim");
+var point4_aim = document.getElementById("point4_aim");
+
+function myFunction1() {
+    var point1_aim = document.getElementById("point1_aim");
+    point1_aim.scrollIntoView();
+  }
+
+  function myFunction2() {
+    var point2_aim = document.getElementById("point2_aim");
+    point2_aim.scrollIntoView();
+  }
+
+  function myFunction3() {
+    var point3_aim = document.getElementById("point3_aim");
+    point3_aim.scrollIntoView();
+  }
+
+  function myFunction4() {
+    var point4_aim = document.getElementById("point4_aim");
+    point4_aim.scrollIntoView();
+  }
+
+  function myFunction5() {
+    var point5_aim = document.getElementById("point5_aim");
+    point5_aim.scrollIntoView();
+  }
+
+
 
 var ham = document.querySelector('.ham');
 var nav = document.querySelector('.navigation_menu');
-
-point1.onclick = function () {
-    window.scrollTo(0, 150 );
-    behavior: 'smooth';
-};
-
-point2.onclick = function () {
-    window.scrollTo(0, 650 );
-    behavior: 'smooth';
-};
-
-point3.onclick = function () {
-    window.scrollTo(0, 1150 );
-    behavior: 'smooth';
-};
-
-point4.onclick = function () {
-    window.scrollTo(0, 1600 );
-    behavior: 'smooth';
-};
-
 ham.onclick = function() {
     nav.classList.toggle('hidden');
+    ham.classList.toggle('open');
 };
-
-
-
-//const el = document.getElementsByClassName('.soft_star1');
-//el.scrollIntoView({block: "center", inline: "center"}); // Задаём параметр block для вертикальной прокрутки до центра, inline до горизонтальной
